38188dcf974640bfb4a0b55d96ecce9e9443b8ac
Tristan <tristanvipero@gmail.com>
2022-02-14 18:20:44 +0800

    code

--- tit/main/support/Forms.py
+++ tit/main/support/Forms.py
@@ -1,10 +1,11 @@
 from wtforms import Form, StringField, RadioField, SelectField, TextAreaField, validators
 from wtforms.fields import EmailField, DateField
 class CreateFeedbackForm(Form):
-    first_name = StringField('First Name', [validators.Length(min=1, max=150), validators.DataRequired()])
-    last_name = StringField('Last Name', [validators.Length(min=1, max=150), validators.DataRequired()])
+    Name = StringField('Name', [validators.Length(min=1, max=150), validators.DataRequired()])
     email = EmailField('Email', [validators.Email(), validators.DataRequired()])
+    type = SelectField('Type', [validators.DataRequired()], choices=[('', 'Select'), ('Accounts', 'Accounts'), ('Payment', 'Payment'),('Shipping','Shipping'),('Others', 'Others')], default='')
     feedback = TextAreaField('Feedback', [validators.DataRequired()])
+    status = SelectField('Status', [validators.DataRequired()], choices=[('Pending', 'Pending'),('Closed', 'Closed')], default='Pending')
 class CreateAccountFeedback(Form):
     account_name = StringField('Account Name', [validators.Length(min=1, max=150), validators.DataRequired()])
     feedback = TextAreaField('Feedback', [validators.DataRequired()])
